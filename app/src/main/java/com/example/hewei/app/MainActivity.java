ee7a9ba4101f6e50011c1202960263184d5d6c78
KingJames6 <xt134520>
2018-07-30 20:25:08 +0800

    first commit

---
+++ app/src/main/java/com/example/hewei/app/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.hewei.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
